0ab2ad102ce20fc07f26ba68c7287b8f1befb277
ladislav-moravec <118938709+ladislav-moravec@users.noreply.github.com>
2023-03-09 19:10:46 +0100

    Initial commit

---
+++ ptak.py
@@ -0,0 +1,16 @@
+class Ptak():
+    #Trida reprezentuje ptaka
+    hlad = 100
+
+    vaha = 50
+
+    #sni jidlo, coz snici hlad a zvysi vahu
+    def snez(self, gramu):
+        self.vaha += gramu
+        self.hlad += gramu
+        if self.hlad < 0:
+            self.hlad = 0
+
+    # vrati textovou reprezentaci ptaka
+    def __str__(self):
+        return "Jsem pták s váhou {} a hladem {}.".format(self.vaha, self.hlad)
